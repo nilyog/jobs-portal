484b300c325afb78b295205153f215c5b230e33c
Bytelogica <Bytelogica>
2015-12-30 11:14:18 +0530

    add new login modal

--- application/views/header.php
+++ application/views/header.php
@@ -32,11 +32,105 @@
               <li><a href="<?php echo base_url(); ?>recruiters">Recruiters</a></li>
                <li><a href="<?php echo base_url(); ?>companies">Companies</a></li>
               <!-- Dropdown Trigger -->
-              <li><a class="dropdown-button" href="#!" data-activates="subMenu">Login<i class="material-icons right">arrow_drop_down</i></a></li>
+              <li><a class="waves-effect waves-light modal-trigger" href="#login_modal">Login</a></li>
+              <!-- <li><a class="dropdown-button" href="#!" data-activates="subMenu">Login<i class="material-icons right">arrow_drop_down</i></a></li> -->
             </ul>
           </div>
         </nav>
       </div>
     </header>
+
+  <!-- Login Modal Structure -->
+  <div id="login_modal" class="modal login">
+    <div class="modal-content">
+      <h6 class="text-center">Jonseeker / Company Login</h6>
+      <h6> &nbsp;</h6>
+      <div class="row">
+          <div class="input-field col s12">
+              <input id="username" type="email" class="validate" name="username" required>
+              <label for="username">Email id</label>
+          </div>
+          <div class="input-field col s12">
+            <input id="login_pwd" type="password" class="validate" name="login_pwd" required>
+            <label for="login_pwd"  >Password</label>
+          </div>
+          
+      </div>  
+    </div>
+    <div class="modal-footer">
+      <div class="row">
+          <div class="col s6 offset-s6 ">
+            <a href="" class=" modal-action modal-close waves-effect waves-green btn ">Login</a>
+          </div>
+          <div class="col s6"> 
+            <h6 class="pull-left"> Not yet register <a href="" class="modal-action modal-close modal-register"> Register </a> </h6>
+          </div>
+          <div class="col s6 ">
+           <h6 class="pull-right forgotpwd"> <a href="" class="modal-action modal-close modal-forgotpwd" href="#forgotPWD_modal" >  Forgot Password? </a> </h6>
+          </div>
+      </div>
+    </div>
+  </div>
+
+<!-- Register Modal Structure -->
+  <div id="register_modal" class="modal register">
+    <div class="modal-content">
+      <h6 class="text-center">Jonseeker / Company Register</h6>
+      <h6> &nbsp;</h6>
+      <div class="row">
+          <div class="input-field col s12">
+              <input id="reg_email" type="email" class="validate" name="reg_email" required>
+              <label for="reg_email">Email id</label>
+          </div>
+          <div class="input-field col s12">
+            <input id="pwd_org" type="password" class="validate" name="pwd_org" required>
+            <label for="pwd_org"  >Password</label>
+          </div>
+          <div class="input-field col s12">
+            <input id="reg_pwd" type="password" class="validate" name="reg_pwd" required>
+            <label for="reg_pwd"  >Re Password</label>
+          </div>
+          
+      </div>  
+    </div>
+    <div class="modal-footer">
+      <div class="row">
+          <div class="col s6 offset-s6 ">
+            <a href="#!" class=" modal-action modal-close waves-effect waves-green btn ">Login</a>
+          </div> 
+      </div>
+    </div>
+  </div>
+
+<!-- Forgot password Modal Structure -->
+  <div id="forgotPWD_modal" class="modal forgotpwd">
+    <div class="modal-content">
+      <h6 class="text-center">Forgot Password</h6>
+      <h6> &nbsp;</h6>
+      <div class="row">
+          <div class="input-field col s12">
+              <input id="reg_email" type="email" class="validate" name="reg_email" required>
+              <label for="reg_email">Email id</label>
+          </div>
+          <div class="input-field col s12">
+            <input id="pwd_org" type="password" class="validate" name="pwd_org" required>
+            <label for="pwd_org"  >Password</label>
+          </div>
+          <div class="input-field col s12">
+            <input id="reg_pwd" type="password" class="validate" name="reg_pwd" required>
+            <label for="reg_pwd"  >Re Password</label>
+          </div>
+      </div>  
+    </div>
+    <div class="modal-footer">
+      <div class="row">
+          <div class="col s6 offset-s6 ">
+            <a href="#!" class=" modal-action modal-close waves-effect waves-green btn ">Submit</a>
+          </div> 
+      </div>
+    </div>
+  </div>
+
+
 
   
